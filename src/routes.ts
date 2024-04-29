ddc791ae881a6462bef69f2510f19aa54a51f584
Ethan Davies <github@ethandavies.co.uk>
2024-04-29 17:07:58 +0100

    feat: recursively load routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
-
-import Hello from './routes/hello'
-
+import path from 'path'
+import fs from 'fs'
 export default class Routes {
     private router: Router
 
@@ -11,7 +10,40 @@ export default class Routes {
     }
 
     private initRoutes() {
-        this.router.use('/', Hello)
+        const routePath = path.join(__dirname, 'routes')
+        this.loadRoutes(routePath)
+    }
+
+    private loadRoutes(routePath) {
+        const items = fs.readdirSync(routePath)
+
+        items.forEach((item) => {
+            const itemPath = path.join(routePath, item)
+            const stats = fs.statSync(itemPath)
+
+            if (stats.isDirectory()) {
+                this.loadRoutes(itemPath)
+            } else if (stats.isFile()) {
+                if (item.endsWith('.js') || item.endsWith('.ts')) {
+
+                    if(item.toLowerCase().startsWith('root')) {
+                        this.router.use('/', require(itemPath).default)
+                        return;
+                    }
+
+                    const modulePath = itemPath.replace(/\.[A-z]{1,2}/gm, '')
+
+                    const routeModule = require(modulePath).default
+
+                    const mountPath = path
+                        .relative(path.join(__dirname, 'routes'), modulePath)
+                        .replace(/\\/g, '/')
+                        .toLowerCase()
+
+                    this.router.use(`/${mountPath}`, routeModule)
+                }
+            }
+        })
     }
 
     public getRouter() {
